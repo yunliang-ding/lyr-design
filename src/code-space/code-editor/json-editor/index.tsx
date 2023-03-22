bd0a06d5cc60845cd7d044e64e950a3f6af6ca84
lighthouse-8G <lighthouse@8g.com>
2023-03-22 14:09:30 +0800

    fixed

--- src/code-space/code-editor/json-editor/index.tsx
+++ src/code-space/code-editor/json-editor/index.tsx
@@ -1,15 +1,24 @@
 import { CodeEditor, CodeProps } from '../index';
 import { debounce } from 'lodash';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './index.less';
 
 export default ({
   value,
   onChange = () => {},
   style = { height: 300 },
+  codeRef = useRef({}),
   ...rest
 }: CodeProps) => {
   const [errorInfo, setErrorInfo] = useState('');
+  const valueRef = useRef(JSON.stringify(value, null, 2));
+  useEffect(() => {
+    Object.assign(codeRef.current, {
+      getJson2Object: () => {
+        return JSON.parse(valueRef.current.replaceAll?.('\n', ''));
+      },
+    });
+  }, []);
   return (
     <div className="json_data_box" style={style}>
       {errorInfo && <div className="json_data_error_info">{errorInfo}</div>}
@@ -21,6 +30,7 @@ export default ({
         onChange={debounce((code: string) => {
           try {
             const options = JSON.parse(code.replaceAll?.('\n', ''));
+            valueRef.current = code;
             onChange(options);
             setErrorInfo('');
           } catch (error) {
