d7c70fd27db4743d2bbaac0496303d20dcb0ab52
lighthouse-8G <lighthouse@8g.com>
2023-03-27 14:28:18 +0800

    fixed

--- src/cloud-component/index.tsx
+++ src/cloud-component/index.tsx
@@ -6,6 +6,8 @@ import { babelParse, Button, CreateSpin } from '../index';
 import Menus from './menus';
 import Tabs from './tabs';
 import Main, { injectStyle } from './main';
+import { downloadFile } from 'react-core-form-tools';
+import { message, Upload } from 'antd';
 import './index.less';
 
 const { open, close } = CreateSpin({
@@ -25,7 +27,7 @@ const CloudComponent = ({
   const [component, setComponent]: any = React.useState(initialComponent);
   const runApi = async () => {
     const current = component.find((i) => i.selected);
-    current?.runApi();
+    current?.runApi?.();
   };
   // Ctrl + S
   const keyboardEvent = async (e) => {
@@ -74,12 +76,66 @@ const CloudComponent = ({
                 <Button spin type="primary" size="small" onClick={runApi}>
                   预览
                 </Button>
-                <Button spin type="primary" size="small">
+                <Button
+                  spin
+                  type="primary"
+                  size="small"
+                  onClick={async () => {
+                    const url = URL.createObjectURL(
+                      new Blob(JSON.stringify(component, null, 2).split('')),
+                    );
+                    await downloadFile(
+                      url,
+                      `${new Date().toLocaleTimeString()}.json`,
+                    );
+                  }}
+                >
                   导出
                 </Button>
-                <Button spin type="primary" size="small">
-                  导入
-                </Button>
+                <Upload
+                  accept=".json"
+                  itemRender={() => null}
+                  onChange={async ({ file }) => {
+                    if (file.status === 'done') {
+                      open();
+                      await new Promise((res) => setTimeout(res, 1000));
+                      try {
+                        const jsonArr = JSON.parse(
+                          await file.originFileObj.text(),
+                        );
+                        if (Array.isArray(jsonArr)) {
+                          // 去重
+                          jsonArr.forEach((jsonItem) => {
+                            // 剔除部分属性
+                            delete jsonItem.open;
+                            delete jsonItem.selected;
+                            if (
+                              !component.some((comp) => {
+                                return (
+                                  comp.componentName === jsonItem.componentName
+                                );
+                              })
+                            ) {
+                              console.log(jsonItem);
+                              component.push(jsonItem);
+                            }
+                          });
+                          setComponent([...component]);
+                        } else {
+                          message.warning('导入失败');
+                        }
+                      } catch (err) {
+                        message.warning(err);
+                      } finally {
+                        close();
+                      }
+                    }
+                  }}
+                >
+                  <Button type="primary" size="small">
+                    导入
+                  </Button>
+                </Upload>
               </div>
             </div>
             {component.map((item) => {
@@ -102,9 +158,9 @@ const parseCodeToReactComponent = (codes: any[], less = window.less) => {
       code: `
       ${code.react} \n;
       // 这里开始注入css样式
-      require('injectStyle')('${code.componentName}', \`${await less.render(
-        code.less,
-      ).css}\`);
+      require('injectStyle')('${code.componentName}', \`${
+        (await less.render(code.less)).css
+      }\`);
 `,
     });
   });
