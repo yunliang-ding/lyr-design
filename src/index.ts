936cf1cab5dc856193b463a534feb0f35450ca31
xulei <xulei@lei.com>
2023-12-07 17:21:57 +0800

    fixed

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ export { default as AnchorCard } from './anchor-card';
 export { default as OssFileUpload } from './oss-file-upload';
 export { default as Suspend } from './suspend';
 export { default as AppLayout } from './app-layout';
-// export { default as MarkdownViewer } from './markdown-viewer';
 export { default as createOssInstance } from './oss-file-upload/util';
 /** 类型 */
 export type { SchemaProps, FieldProps } from './form/type.item';
