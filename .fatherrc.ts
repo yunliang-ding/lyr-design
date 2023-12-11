a5f3ae9940d5665bca916a25bcd71346c5c900ee
xulei <xulei@lei.com>
2023-12-11 11:20:13 +0800

    fixed

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,25 +1,22 @@
 // 配置参考 https://github.com/umijs/father/tree/father-build@1.18.2
-export default process.env.type === 'umd'
-  ? {
-      umd: {
-        name: 'ReactCoreForm',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          '@arco-design/web-react': 'arco',
-          '@arco-design/web-react/icon': 'arcoicon',
-        },
-      },
-      extractCSS: true,
-    }
-  : {
-      esm: {
-        type: 'rollup',
-        minify: true,
-      },
-      cjs: {
-        type: 'rollup',
-        minify: true,
-      },
-      extractCSS: true,
-    };
+
+export default {
+  esm: {
+    type: 'rollup',
+    minify: true,
+  },
+  cjs: {
+    type: 'rollup',
+    minify: true,
+  },
+  umd: {
+    name: 'ReactCoreForm',
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      '@arco-design/web-react': 'arco',
+      '@arco-design/web-react/icon': 'arcoicon',
+    },
+  },
+  extractCSS: true,
+};
